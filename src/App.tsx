c993d0bdbd0a550d9a13fc78ec9cd060d6fbe5ea
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-01-14 15:00:26 +0600

    created responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-	return <Button colorScheme="blue">Button</Button>;
+	return (
+		<Grid
+			templateAreas={{
+				base: `
+				"nav"
+				"main"
+			`,
+				lg: `
+				"nav nav"
+				"main aside"
+			`,
+			}}
+		>
+			<GridItem gridArea="nav" bg="tomato" h="100px">
+				nav
+			</GridItem>
+			<Show above="lg">
+				<GridItem gridArea="aside" bg="papayawhip" h="100px">
+					aside
+				</GridItem>
+			</Show>
+			<GridItem gridArea="main" bg="pink" h="100px">
+				main
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
